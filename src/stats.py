3be0ef899eb1a152b2cfa16be040fc7c72625931
Mickaïl Khadhar <micka.khadhar@gmail.com>
2023-02-18 22:57:19 +0100

    update for flake8 convention

--- src/stats.py
+++ src/stats.py
@@ -1,9 +1,11 @@
 import numpy as np
+
+
 class Stats:
     def __init__(self):
         pass
 
-# apply min max normalization of all the column needed
+    # apply min max normalization of all the column needed
     def normalize(self, data, cols_to_norm):
         try:
             data[cols_to_norm] = data[cols_to_norm].apply(lambda x: (x - x.min()) / (x.max() - x.min()))
@@ -11,14 +13,14 @@ class Stats:
             print(f'Error : {str(e)}')
         return data
 
-#return max of column
+    # return max of column
     def get_max(self, data, column):
         return max(data[column])
 
-    #return min of column
+    # return min of column
     def get_min(self, data, column):
         return min(data[column])
 
-    #return variance of column
+    # return variance of column
     def get_variance(self, data, column):
         return np.var(data[column])
